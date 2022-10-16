fa8e5e2c5bf8c6d2e68013824fc7f7e7c40dc805
trenod <torewb@gmail.com>
2022-10-16 17:23:23 +0200

    Working on error handling in createAgent and updateAgent, trying to fix delete in Postman.

--- src/main/java/com/hvl/feedApp/service/AgentService.java
+++ src/main/java/com/hvl/feedApp/service/AgentService.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class AgentService {
@@ -35,6 +32,20 @@ public class AgentService {
     }
 
     public Agent createNewAgent(Agent agent) {
+        if (agent.getUsername().isEmpty() || agent.getUsername().isBlank()) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if (agent.getPassword().isEmpty() || agent.getPassword().isBlank()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+        if (agent.getEmail().isEmpty() || agent.getEmail().isBlank()) {
+            throw new IllegalArgumentException("Email cannot be empty");
+        }
+        Role role = agent.getRole();
+        if (role == null && !allowedRoles.contains(role)) {
+            throw new IllegalStateException("Role must be one of three strings: 'USER', 'ADMIN' or 'DEVICE', not: "+role);
+        }
+
         agentRepository.save(agent);
         return this.getById(agent.getAgentID());
     }
@@ -46,8 +57,9 @@ public class AgentService {
 //            vote.setVoter(null);
 //        }
 
-        boolean exists = agentRepository.existsById(agentID);
-        if (!exists) {
+        Optional<Agent> agentToDelete = agentRepository.findById(agentID);
+        //boolean exists = agentRepository.existsById(agentID);
+        if (agentToDelete.isEmpty()) {
             throw new IllegalStateException("User with id: " + agentID + " does not exist");
         }
         agentRepository.deleteById(agentID);
@@ -61,6 +73,18 @@ public class AgentService {
         JsonObject body = new Gson().fromJson(bodyString, JsonObject.class);
 
         //TODO: Error handling, field validation!
+        if (body.get("username").getAsString().isEmpty() || body.get("username").getAsString().isBlank()) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if (body.get("email").getAsString().isEmpty() || body.get("email").getAsString().isBlank()) {
+            throw new IllegalArgumentException("Email cannot be empty");
+        }
+        if (body.get("password").getAsString().isEmpty() || body.get("password").getAsString().isBlank()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+        if (body.get("role").getAsString().isEmpty() || body.get("role").getAsString().isBlank()) {
+            throw new IllegalArgumentException("Role cannot be empty");
+        }
         String username = body.get("username").getAsString();
         String email = body.get("email").getAsString();
         String password = body.get("password").getAsString();
